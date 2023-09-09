06a413c4479c4f19da2fafc52992c06f9c7fcfbe
takaya <tky.ishimori@gmail.com>
2023-09-09 16:18:29 +0900

    再生・停止の切り替えボタンが完成

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        switchBtn.setTitle("再生", for: .normal)
     }
-
-
+    
+    @IBOutlet weak var switchBtn: UIButton!
+    
+    @IBAction func switchBtn(_ sender: UIButton) {
+        if var title = switchBtn.currentTitle {
+            if title == "再生" {
+                switchBtn.setTitle("停止", for: .normal)
+            } else {
+                switchBtn.setTitle("再生", for: .normal)
+            }
+        } else {
+            print("nilです")
+        }
+    }
+    
+    
+    
 }
 
