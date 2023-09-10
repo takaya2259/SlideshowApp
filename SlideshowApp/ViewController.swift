0ed5df5a139a77842d4809622f2587603b1034ec
takaya <tky.ishimori@gmail.com>
2023-09-10 11:16:24 +0900

    進む・戻るボタンの作成完了

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+extension UIImageView {
+    func getFileName() -> String? {
+        return self.image?.accessibilityIdentifier
+    }
+}
+
 class ViewController: UIViewController {
     
     // アウトレット接続
@@ -23,6 +29,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         switchBtn.setTitle("再生", for: .normal)
         image.image = istanbulImage
+        image.image?.accessibilityIdentifier = "istanbul.jpg"
     }
     
     // 再生・停止切り替え
@@ -37,16 +44,59 @@ class ViewController: UIViewController {
             print("nilです")
         }
     }
-    
     // 進む
     @IBAction func forward(_ sender: UIButton) {
-        let folder = [istanbulImage, parisImage, venetiaImage]
+        let folder = ["istanbul.jpg", "paris.jpg", "venetia.jpg"]
+        // 現在表示中のjpg情報
+        let photoname = image.getFileName()
         
+        // 現在表示中のjpg番号
+        func identifier() -> Int {
+            var ans:Int?
+            for i in 0 ... 2 {
+                if folder[i] == photoname {
+                    ans = i
+                } else {
+                    continue
+                }
+            }
+            return ans!
+        }
+        var num:Int = identifier()
+        // 次表示すべき画像の番号
+        var nextnum:Int = num - 1
+        if nextnum == -1 {
+            image.image = venetiaImage
+            image.image?.accessibilityIdentifier = "venetia.jpg"
+        } else {
+            image.image = UIImage(named: folder[nextnum % 3])
+            image.image?.accessibilityIdentifier = folder[nextnum % 3]
+        }
     }
     // 戻る
     @IBAction func back(_ sender: UIButton) {
-        let folder = [istanbulImage, parisImage, venetiaImage]
+        let folder = ["istanbul.jpg", "paris.jpg", "venetia.jpg"]
+        // 現在表示中のjpg情報
+        let photoname = image.getFileName()
         
+        // 現在表示中のjpg番号
+        func identifier() -> Int {
+            var ans:Int?
+            for i in 0 ... 2 {
+                if folder[i] == photoname {
+                    ans = i
+                } else {
+                    continue
+                }
+            }
+            return ans!
+        }
+        var num:Int = identifier()
+        // 次表示すべき画像の番号
+        var nextnum:Int = num + 1
+        image.image = UIImage(named: folder[nextnum % 3])
+        image.image?.accessibilityIdentifier = folder[nextnum % 3]
     }
 }
+
 
